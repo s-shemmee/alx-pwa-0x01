c1410cc1a59c72fdc98b0c1cca61d97e0bc2fe96
s-shemmee <56132945+s-shemmee@users.noreply.github.com>
2025-08-23 03:23:48 +0100

    feat: configure PWA support with manifest and icons, update TypeScript version

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -2,7 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    turbo: false,
+  },
   reactStrictMode: true,
+  images: {
+    domains: ['m.media-amazon.com'],
+  },
 };
 
-export default nextConfig;
+import withPWAInit from "@ducanh2912/next-pwa";
+
+/** @type {import('next').NextConfig} */
+
+const withPWA = withPWAInit({
+  dest: 'public'
+});
+
+export default withPWA({
+  ...nextConfig
+});
