a82211d82d5cc2a35f07e670d367fc7cf846fd58
s-shemmee <56132945+s-shemmee@users.noreply.github.com>
2025-08-24 20:54:30 +0100

    Refactor code structure for improved readability and maintainability

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    turbo: false,
-  },
   reactStrictMode: true,
   images: {
     domains: ['m.media-amazon.com'],
